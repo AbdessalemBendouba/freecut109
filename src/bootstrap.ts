2ca6fd76fa8a0b73aafd86efd856112764513967
Bendouba Abdessalem <moi.cest.salamo@gmail.com>
2026-07-27 19:02:31 +0100

    Implement polyfills for ES2023/ES2024 features
    Added polyfills for new Array and Promise methods to support older environments.

--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -1,3 +1,48 @@
+// Chrome 109 ES2023/ES2024 Polyfills
+if (!(Array.prototype as any).toSorted) {
+  ;(Array.prototype as any).toSorted = function (compareFn?: (a: any, b: any) => number) {
+    return [...this].sort(compareFn)
+  }
+}
+if (!(Array.prototype as any).toReversed) {
+  ;(Array.prototype as any).toReversed = function () {
+    return [...this].reverse()
+  }
+}
+if (!(Array.prototype as any).toSpliced) {
+  ;(Array.prototype as any).toSpliced = function (
+    start: number,
+    deleteCount?: number,
+    ...items: any[]
+  ) {
+    const copy = [...this]
+    copy.splice(start, deleteCount === undefined ? copy.length - start : deleteCount, ...items)
+    return copy
+  }
+}
+if (!(Array.prototype as any).with) {
+  ;(Array.prototype as any).with = function (index: number, value: any) {
+    const copy = [...this]
+    copy[index] = value
+    return copy
+  }
+}
+if (!Promise.withResolvers) {
+  ;(Promise as any).withResolvers = function () {
+    let resolve: (value: unknown) => void, reject: (reason?: unknown) => void
+    const promise = new Promise((res, rej) => {
+      resolve = res
+      reject = rej
+    })
+    return { promise, resolve: resolve!, reject: reject! }
+  }
+}
+if (!Object.hasOwn) {
+  Object.hasOwn = function (obj: object, prop: PropertyKey) {
+    return Object.prototype.hasOwnProperty.call(obj, prop)
+  }
+}
+
 const APP_SHELL_CACHE_PREFIX = 'freecut-app-shell-'
 
 async function removeProductionAppShellFromDevelopment(): Promise<boolean> {
